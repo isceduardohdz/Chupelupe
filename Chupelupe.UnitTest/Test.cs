1d764b8b594c8b87aca468279612e997d30259f0
Juan Eduardo HErnandez Villegas <tec.eduardohdz@outlook.com>
2019-01-28 18:32:45 -0600

    FirstCom

---
+++ Chupelupe.UnitTest/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Chupelupe.UnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
